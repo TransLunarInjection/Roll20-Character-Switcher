3296a6507a5f666481cbe6d737ed8b3932d3791c
Björn Büttner <Idrinth@users.noreply.github.com>
2017-02-03 14:52:30 +0100

    earlier return for easier reading

--- Roll20-Character-Switcher.user.js
+++ Roll20-Character-Switcher.user.js
@@ -2,7 +2,7 @@
 // @name         Roll20 Character Switcher
 // @namespace    de.idrinth
 // @homepage     https://github.com/Idrinth/Roll20-Character-Switcher
-// @version      1.0.2
+// @version      1.0.3
 // @description  Switches the chatting character to the one who's sheet you clicked on
 // @author       Idrinth
 // @match        https://app.roll20.net/editor/
@@ -14,21 +14,22 @@
     var a = function() {
         document.getElementsByTagName('body')[0].addEventListener('mousedown', function(event) {
             var e = window.event || event;
-            if (e.target.tagName === 'BUTTON' && e.target.hasAttribute('type') && e.target.getAttribute('type') === 'roll') {
-                var character = e.target;
-                while (!character.hasAttribute('data-characterid')) {
-                    if(!character.parentNode) {
-                        return;
-                    }
-                    character = character.parentNode;
+            if (e.target.tagName !== 'BUTTON' || !e.target.hasAttribute('type') || e.target.getAttribute('type') !== 'roll') {
+                return;
+            }
+            var character = e.target;
+            while (!character.hasAttribute('data-characterid')) {
+                if(!character.parentNode) {
+                    return;
                 }
-                var id = 'character|' + character.getAttribute('data-characterid');
-                var select = document.getElementById('speakingas');
-                for (var i = 0; i < select.options.length; i++) {
-                    if (select.options[i].value === id) {
-                        select.selectedIndex = i;
-                        return;
-                    }
+                character = character.parentNode;
+            }
+            var id = 'character|' + character.getAttribute('data-characterid');
+            var select = document.getElementById('speakingas');
+            for (var i = 0; i < select.options.length; i++) {
+                if (select.options[i].value === id) {
+                    select.selectedIndex = i;
+                    return;
                 }
             }
         });
