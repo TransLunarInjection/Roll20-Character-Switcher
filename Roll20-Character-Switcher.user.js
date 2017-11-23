d85f695d8a63b8ca25560dbffdcff6d09bb30280
KingMarth <KingMarth@users.noreply.github.com>
2017-11-22 22:36:02 -0700

    Adding support for token actions
    The default event listeners on token actions prevent click events
    from propagating, so I changed where the event listeners are
    registered. I also removed unnecessary use of eval and nesting.

--- Roll20-Character-Switcher.user.js
+++ Roll20-Character-Switcher.user.js
@@ -2,45 +2,51 @@
 // @name         Roll20 Character Switcher
 // @namespace    de.idrinth
 // @homepage     https://github.com/Idrinth/Roll20-Character-Switcher
-// @version      1.1.0
-// @description  Switches the chatting character to the one who's sheet or macro you clicked on
-// @author       Idrinth
+// @version      1.2.0
+// @description  Switches the chatting character to the one whose sheet or macro you clicked on
+// @author       Idrinth, KingMarth
 // @match        https://app.roll20.net/editor/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var a = function() {
-        document.getElementsByTagName('body')[0].addEventListener('mousedown', function(event) {
-            var e = window.event || event;
-            if (e.target.tagName !== 'BUTTON') {
-                return;
-            }
-            var id='';
-            if(e.target.hasAttribute('type') && e.target.getAttribute('type') === 'roll') {
-                var character = e.target;
-                while (!character.hasAttribute('data-characterid')) {
-                    if(!character.parentNode) {
-                        return;
-                    }
-                    character = character.parentNode;
-                }
-                id = 'character|' + character.getAttribute('data-characterid');
-            } else if(e.target.hasAttribute('class') && e.target.getAttribute('class') === 'btn' && e.target.parentNode.hasAttribute('data-macroid')) {
-                id = 'character|' + (e.target.parentNode.getAttribute('data-macroid')).split('|')[0];
-            }
-            if(!id) {
-                return;
-            }
-            var select = document.getElementById('speakingas');
-            for (var i = 0; i < select.options.length; i++) {
-                if (select.options[i].value === id) {
-                    select.selectedIndex = i;
+    var charswitcher = function(event) {
+        var e = window.event || event;
+        if (e.target.tagName !== 'BUTTON') {
+            return;
+        }
+        var id='';
+        if(e.target.hasAttribute('type') && e.target.getAttribute('type') === 'roll') {
+            var character = e.target;
+            while (!character.hasAttribute('data-characterid')) {
+                if(!character.parentNode) {
                     return;
                 }
+                character = character.parentNode;
             }
-        });
+            id = 'character|' + character.getAttribute('data-characterid');
+        } else if(e.target.hasAttribute('class') && e.target.getAttribute('class') === 'btn') {
+            var macro = e.target;
+            while (!macro.hasAttribute('data-macroid')) {
+                if(!macro.parentNode) {
+                    return;
+                }
+                macro = macro.parentNode;
+            }
+            id = 'character|' + (macro.getAttribute('data-macroid')).split('|')[0];
+        }
+        if(!id) {
+            return;
+        }
+        var select = document.getElementById('speakingas');
+        for (var i = 0; i < select.options.length; i++) {
+            if (select.options[i].value === id) {
+                select.selectedIndex = i;
+                return;
+            }
+        }
     };
-    eval('(' + a.toString() + '())');
+    document.getElementsByTagName('body')[0].addEventListener('mousedown', charswitcher);
+    document.getElementsByClassName('tokenactions')[0].addEventListener('mousedown', charswitcher);
 })();
